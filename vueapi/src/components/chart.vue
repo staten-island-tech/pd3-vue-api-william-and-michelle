d555ca8480387edcebcf826cb0686f9cf0b4c3d9
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-04-25 10:03:43 -0400

    no browser

--- vueapi/src/components/chart.vue
+++
@@ -1,1 +0,0 @@
-import { Bar } from chart.js
---
+++ vueapi/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const names = ref('')
+async function getNames() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
+  let data = await res.json()
+  names.value.values = data.results
+}
+onMounted(() => {
+  getNames()
+})
+</script>
+
+<style scoped></style>
