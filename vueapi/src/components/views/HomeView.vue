e63d119b786ab7fcd1dc30d723763ab62bb394ab
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-04-26 10:12:17 -0400

    ummmmm

---
+++ vueapi/src/components/views/HomeView.vue
@@ -0,0 +1,24 @@
+A
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+const name = ref('')
+async function getName() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
+  data.value = await res.json()
+
+  let 2019 = data.filter(year)
+  let sorted2019 = 2019.sort()
+  name.value = sorted2019
+}
+
+onMounted(() => {
+  getName()
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
