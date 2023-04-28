b10c12513d9d883defd051c1436c82fe34ad1d89
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-04-28 10:16:06 -0400

    im so done

--- vueapi/src/main.js
+++ vueapi/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './components/router'
 
 import './assets/main.css'
 
 const app = createApp(app)
 
 App.use(router)
+
 createApp(App).mount('#app')
