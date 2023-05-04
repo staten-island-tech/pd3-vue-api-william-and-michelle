fd4003b2dd622ec0d4580440bd4ead4c7d82d5b7
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-05-04 09:53:56 -0400

    idek

--- vueapi/src/router/index.js
+++ vueapi/src/router/index.js
@@ -17,14 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
-    {
+    /* {
       path: '/BarChart',
       name: 'BarChart',
       component: () => import('../views/BarChart.vue')
-    },
+    }, */
     {
-      path: '/BabyData',
-      name: 'BabyData',
+      path: '/BabyData/:id',
+      name: 'bbPath',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
