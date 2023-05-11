b4b86e723d32dc2f869224a4b905d22c107f76fe
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-05-11 10:11:57 -0400

    Final

--- vueapi/src/components/DoughnutChart.vue
+++ vueapi/src/components/DoughnutChart.vue
@@ -18,7 +18,19 @@ export default {
     },
     loaded: false,
     chartData: {
-      labels: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th'],
+      labels: [
+        'Chloe',
+        'Olivia',
+        'Mia',
+        'Sophia',
+        'Emma',
+        'Amelia',
+        'Charlotte',
+        'Emily',
+        'Grace',
+        'Isabella',
+        'Ella'
+      ],
       datasets: [
         {
           label: 'Count of Each Ranked Name',
--- vueapi/src/views/DoughnutChartView.vue
+++ vueapi/src/views/DoughnutChartView.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h1>Top 5 Baby Names</h1>
+    <h1>Top 10 Baby Names</h1>
     <DoughnutChart></DoughnutChart>
   </div>
 </template>
 <script setup>
 import { ref, onMounted } from 'vue'
 import DoughnutChart from '../components/DoughnutChart.vue'
-const stuff = ref('') //with ref, if we need some data to update regularly like with an async function, ref lets you
+const babies = ref('') //with ref, if we need some data to update regularly like with an async function, ref lets you
 async function getbabies() {
   let result = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
   let data = await result.json()
